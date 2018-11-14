2b042f9a12ade7291854704a076bafab7b6ab024
Nathan Peck <nathan.peck@okstate.edu>
2018-11-13 23:08:38 -0700

    push 11-13-18

--- trivia.js
+++ trivia.js
@@ -3,32 +3,44 @@
 var startButton = document.getElementById("startButton");
 
 // array of objects for trivia questions and answers
-var questions = [questionOne, questionTwo, questionThree]
-// trivia questions and answers
-var questionOne = {
-    question: "Some elves work out of the south pole.",
-    answer: "true"
-}
-var questionTwo = {
-    question: "Elves can grow over 4 feet tall.",
-    answer: "false"
-}
-var questionThree = {
-    question: "Elves can live to be 1379 years old.",
-    answer: "true"
-}
+var questions = [
+    {
+        "trivia": "Some elves work out of the south pole.",
+        "answer": "true"
+    },
+    {
+        "trivia": "Elves can grow over 4 feet tall.",
+        "answer": "false"
+    },
+    {
+        "trivia": "Elves can live to be 1379 years old.",
+        "answer": "true"
+    }]
 
 
 
-startButton.onclick = function(questions){
 
-    // Start and print countdown
-    
-    // Populate Trivia questions
+startButton.onclick = function() {
+
+// Begin and print countdown
+
+    // var timeRemaining = 5;
+    // $("#countdown").append("<div>"+"<h3>" + "You have " + timeRemaining + " seconds to answer!");setTimeout(timeUp, 1000 * 15);
+
+// Populate Trivia questions
     var i;
     for (i=0; i<3; i++){
-        $(".questions").append("<div>"+"Question"+questions[i]);
-}}
+        $(".questions").append("<div>"+questions[i].trivia);
+        $(".questions").append("<div>"+"<button>"+"True"+"<button>"+"False");
+
+    }
+    
+
+
+// function fiveSeconds() {
+    
+// }
+}
 
 
 
